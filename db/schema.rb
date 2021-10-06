39355f1f1dfd39628c76581434404cb2975b1232
Laguna1 <lagunazoomarket@gmail.com>
2021-10-07 00:18:32 +0300

    Add model Event

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_211_006_205_646) do
+ActiveRecord::Schema.define(version: 20_211_006_211_632) do
+  create_table 'events', force: :cascade do |t|
+    t.string 'title'
+    t.string 'place'
+    t.datetime 'date'
+    t.datetime 'created_at', precision: 6, null: false
+    t.datetime 'updated_at', precision: 6, null: false
+  end
+
   create_table 'users', force: :cascade do |t|
     t.string 'email', default: '', null: false
     t.string 'encrypted_password', default: '', null: false
