a7c473932ac0c964d04ccdfb5fcca6127f286df9
Laguna1 <lagunazoomarket@gmail.com>
2021-10-07 00:03:42 +0300

    Add field name to users table

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_211_006_195_131) do
+ActiveRecord::Schema.define(version: 20_211_006_205_646) do
   create_table 'users', force: :cascade do |t|
     t.string 'email', default: '', null: false
     t.string 'encrypted_password', default: '', null: false
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20_211_006_195_131) do
     t.datetime 'remember_created_at'
     t.datetime 'created_at', precision: 6, null: false
     t.datetime 'updated_at', precision: 6, null: false
+    t.string 'name'
     t.index ['email'], name: 'index_users_on_email', unique: true
     t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
   end
