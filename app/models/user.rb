27c3d5b74f37ef731154025a928ff3ade1087c91
Laguna1 <lagunazoomarket@gmail.com>
2021-10-07 00:45:24 +0300

    Fix syntax issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :created_events, class_name: 'Event', foreigh_key: 'creator_id', dependent: :destroy
+  has_many :created_events, class_name: 'Event', foreign_key: 'creator_id', dependent: :destroy
   has_many :attendances, dependent: :destroy
   has_many :event_attended, through: :attendances, source: :event
 end
