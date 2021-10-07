a5fa37b5d605e6ef77a3d5e662a49e8b53939a3a
Laguna1 <lagunazoomarket@gmail.com>
2021-10-08 01:08:37 +0300

    Display user\s events counter

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class EventsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+  
   def index
     @events = Event.all.order('created_at DESC')
   end
