783ce35f30ae154d23fc0ef758eb7b274a0d11f4
Laguna1 <lagunazoomarket@gmail.com>
2021-10-09 02:01:38 +0300

    Fix attendances controller

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -2,10 +2,8 @@
 
 class AttendancesController < ApplicationController
   def join
-    unless Attendance.all.exists?(user_id: current_user.id)
-      @attendance = Attendance.new(event_id: params[:id], user_id: current_user.id)
-      @attendance.save
-      redirect_to events_path
-    end
+    @attendance = Attendance.new(event_id: params[:id], user_id: current_user.id)
+    @attendance.save
+    redirect_to events_path
   end
 end
